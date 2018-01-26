a04b548d4638ecc9f86133ec81e4ea933accee52
WindCrowAya <lightingstorm1995@mail.ru>
2018-01-26 21:27:41 +0500

    Fixed bug related to encoding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import sun.nio.cs.StandardCharsets;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -10,13 +12,15 @@ import java.util.*;
  *
  * Useful links:
  *   Arrays.copyOf() : https://stackoverflow.com/questions/1018750/how-to-convert-object-array-to-string-array-in-java
+ *   https://ru.stackoverflow.com/questions/679318/%D0%92%D0%BC%D0%B5%D1%81%D1%82%D0%BE-%D0%B2%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%BD%D1%8B%D1%85-%D1%81%D0%BB%D0%BE%D0%B2-%D0%BE%D1%82%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B0%D1%8E%D1%82%D1%81%D1%8F-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB%D1%8B/679343#679343
  *
  * View https://github.com/WindCrowAya/sample to see the history of the project change
  */
 public class Main {
 
     private static void renameFiles() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in,
+                System.getProperty("console.encoding", "cp866")));
 
         boolean pathIsEmpty,
                 commandIsEmpty,
